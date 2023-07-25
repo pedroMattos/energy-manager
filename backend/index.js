6adc9c6eadaee07830f509e884ca4621bbc66e7b
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-25 20:49:40 -0300

    change migration call

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const api = require('./api')
 const app = express();
-const createTableInvoiceIfNotExists = require('./migrations/createTableInvoice')
-const dataProcessing = require('./dataProcessing')
+require('./migrations/createTableInvoiceAndSartProcess')()
 
-// createTableInvoiceIfNotExists()
-dataProcessing(process.env.FILES_FOLDER_NAME)
 api.getInvoiceData().then((data) => {
   console.log(data)
 })
+
 
 app.listen(process.env.PORT, () => {
   console.log('Backend executando...', process.env.PORT)
