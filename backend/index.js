dcab93be7ff1b64f65e24342059764acfe93f2eb
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-25 01:23:07 -0300

    add node initial config

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const pgDbConnection = require('./pgConfig')
+const dataProcessing = require('./dataProcessing')
+
+app.db = pgDbConnection
+
+// pgDbConnection('Test').insert({ valor: 1123 }).then(() => {
+//   console.log('inseriu')
+// }).catch((error) => {
+//   console.log('erro ao inserir', error)
+// })
+
+// pgDbConnection('Test').select('*').then((value) => {
+//   console.log(value)
+// })
+
+dataProcessing()
+
+app.listen(process.env.PORT, () => {
+  console.log('Backend executando...')
+})
