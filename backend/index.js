2d15e21e3fab4265ce95fff4dae4a3c432ecf248
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-25 15:53:25 -0300

    add migrations and get invoice data

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,16 @@
 const express = require("express");
+require("dotenv").config();
+const api = require('./api')
 const app = express();
-const pgDbConnection = require('./pgConfig')
+const createTableInvoiceIfNotExists = require('./migrations/createTableInvoice')
 const dataProcessing = require('./dataProcessing')
 
-app.db = pgDbConnection
-
-// pgDbConnection('Test').insert({ valor: 1123 }).then(() => {
-//   console.log('inseriu')
-// }).catch((error) => {
-//   console.log('erro ao inserir', error)
-// })
-
-// pgDbConnection('Test').select('*').then((value) => {
-//   console.log(value)
-// })
-
-dataProcessing('files')
+// createTableInvoiceIfNotExists()
+dataProcessing(process.env.FILES_FOLDER_NAME)
+api.getInvoiceData().then((data) => {
+  console.log(data)
+})
 
 app.listen(process.env.PORT, () => {
-  console.log('Backend executando...')
+  console.log('Backend executando...', process.env.PORT)
 })
