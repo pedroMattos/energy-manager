e545be2b91843d404b09084389209e75dfb6e95d
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-26 00:42:40 -0300

    add store, react router dom to index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import store from './config/store'
+import { Provider } from 'react-redux'
 import './index.css';
-import App from './App';
+import App from './views/App';
 import reportWebVitals from './reportWebVitals';
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+    </React.StrictMode>
+    ),
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
