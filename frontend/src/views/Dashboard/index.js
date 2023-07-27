35dc822dd3ecc3c6d4e97a04b47ab11a97099406
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-27 11:54:36 -0300

    import graph cards to dashbpard

--- frontend/src/views/Dashboard/index.js
+++ frontend/src/views/Dashboard/index.js
@@ -1,3 +1,4 @@
+import GraphCard from "../../components/cards/graphCard";
 import MeditionCard from "../../components/cards/meditionCard";
 import { PageTitle } from "../styles";
 import { DashboardBody, DashboardHeader, DashboardMeditions, DashboardSection } from "./styles";
@@ -12,9 +13,14 @@ function Dashboard() {
       <p>Último mês</p>
       <DashboardBody>
         <DashboardMeditions>
-          <MeditionCard title={'Consumo de energia (kWh)'} value={100} />
-          <MeditionCard title={'Consumo de energia (kWh)'} value={100} />
-          <MeditionCard title={'Consumo de energia (kWh)'} value={100} />
+          <MeditionCard title={'Consumo de energia (kWh)'} value={100} unit={'kWh'} />
+          <MeditionCard title={'Consumo de energia (kWh)'} value={100} unit={'R$'} />
+          <MeditionCard title={'Consumo de energia (kWh)'} value={100} unit={'R$'} />
+        </DashboardMeditions>
+        <p>Todos os meses</p>
+        <DashboardMeditions>
+          <GraphCard title={'Valor da Fatura por vencimento'} />
+          <GraphCard title={'Economia de energia compensada ou injetada'} />
         </DashboardMeditions>
       </DashboardBody>
     </DashboardSection>
