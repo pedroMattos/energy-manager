5e45733f7dfe4b821836821f92a3cbef2ea1f81a
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-27 11:04:33 -0300

    dashboard component styles

---
+++ frontend/src/views/Dashboard/styles.js
@@ -0,0 +1,22 @@
+import { styled } from "styled-components";
+
+export const DashboardSection = styled.section`
+  padding: 20px;
+  width: 100%;
+`
+
+export const DashboardMeditions = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 10px;
+  justify-content: center;
+  justify-content: space-between;
+`
+
+export const DashboardBody = styled.div`
+
+`
+
+export const DashboardHeader = styled.div`
+  margin-bottom: 40px;
+`
