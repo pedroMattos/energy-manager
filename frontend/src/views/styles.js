be808c98bf9238b76ed1e2e5f02be5e95ee52ca9
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-27 11:53:32 -0300

    remove echarts style from global styles

--- frontend/src/views/styles.js
+++ frontend/src/views/styles.js
@@ -2,9 +2,6 @@ import { styled } from "styled-components";
 
 export const AppWrapper = styled.section`
   display: flex;
-  .echarts-for-react {
-    min-width: 100px;
-  }
   p {
     margin: 0;
   }
