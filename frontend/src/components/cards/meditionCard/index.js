4d1f95724efb2e83a3d0e0089337a3f165ea4ae9
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-27 01:04:03 -0300

    add card component

---
+++ frontend/src/components/cards/meditionCard/index.js
@@ -0,0 +1,9 @@
+import { CardHeader, CardWrapper } from "./styles"
+
+function MeditionCard() {
+  return <CardWrapper>
+    <CardHeader><p>Consumo de energia (kWh)</p></CardHeader>
+  </CardWrapper>
+}
+
+export default MeditionCard
---
+++ frontend/src/components/cards/meditionCard/styles.js
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const CardHeader = styled.div`
+  padding: 16px;
+  border-bottom: 1px solid #D9D9D9;
+`
+
+export const CardWrapper = styled.div`
+  border-radius: 5px;
+  border: 1px solid #D9D9D9;
+  min-width: 380px;
+  height: 220px;
+`
