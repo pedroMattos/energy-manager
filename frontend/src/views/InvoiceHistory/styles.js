07c9da348bb86a6ad12f2cffe6c51036873b19b2
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-27 22:11:24 -0300

    add styles

--- frontend/src/views/InvoiceHistory/styles.js
+++ frontend/src/views/InvoiceHistory/styles.js
@@ -2,4 +2,16 @@ import { styled } from "styled-components";
 
 export const GroupBySelectors = styled.div`
   display: flex;
+  max-width: 60%;
+  gap: 4px;
+  align-items: center;
+  margin-bottom: 10px;
+  .MuiIconButton-root {
+    height: 40px;
+    width: 40px;
+  }
+`
+
+export const GroupTitle = styled.p`
+  min-width: 100px;
 `
