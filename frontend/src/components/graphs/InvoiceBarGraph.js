8ee923102eaecd1d5c76001824dc7218bf1ba087
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-28 15:16:55 -0300

    change const to useMemo

--- frontend/src/components/graphs/InvoiceBarGraph.js
+++ frontend/src/components/graphs/InvoiceBarGraph.js
@@ -1,32 +1,31 @@
 import ReactECharts from 'echarts-for-react';
 import useGraphData from './hooks/useInvoiceGraphData';
-import { useEffect } from 'react';
-const options = {
-  xAxis: {
-    type: 'category',
-    data: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun']
-  },
-  yAxis: {
-    type: 'value'
-  },
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'shadow'
-    }
-  },
-  series: [
-    { data: [], type: 'bar' }
-  ]
-}
+import { useMemo } from 'react';
 
 function InvoiceBarGraph() {
   const { series, isLoad } = useGraphData()
 
-  useEffect(() => {
-    if (!isLoad) options.series.at(0).data = series
-
-  }, [isLoad, series])
+  const options = useMemo(() => {
+    if (isLoad) return
+    return {
+      xAxis: {
+        type: 'category',
+        data: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow'
+        }
+      },
+      series: [
+        { data: series, type: 'bar' }
+      ]
+    }
+  }, [series, isLoad])
 
   return (
     <>
