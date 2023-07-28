ff3d2dd7f7fbf2a8bbfef29211b8abd1089e6f9f
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-28 15:14:22 -0300

    change const to useMemo

--- frontend/src/components/graphs/MoneySaveGraph.js
+++ frontend/src/components/graphs/MoneySaveGraph.js
@@ -1,35 +1,32 @@
 import ReactECharts from 'echarts-for-react';
-import { useEffect } from 'react';
+import { useMemo } from 'react';
 import useMoneySaveGraphData from './hooks/useMoneySaveGraphData';
-const options = {
-  xAxis: {
-    type: 'category',
-    data: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun']
-  },
-  yAxis: {
-    type: 'value'
-  },
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'shadow'
-    }
-  },
-  series: [
-    { data: [0, 0, 0, 0, 0, 0], type: 'bar', name: 'HFP Injetado' },
-    { data: [0, 0, 0, 0, 0, 0], type: 'bar', name: 'Energia compensada' }
-  ]
-}
 
 function MoneySaveGraph() {
   const { series, isLoad } = useMoneySaveGraphData()
 
-  useEffect(() => {
-    if (!isLoad) {
-      options.series.at(0).data = series.hfp
-      options.series.at(1).data = series.compensated
+  const options = useMemo(() => {
+    if (isLoad) return
+    return {
+      xAxis: {
+        type: 'category',
+        data: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow'
+        }
+      },
+      series: [
+        { data: series.hfp, type: 'bar', name: 'HFP Injetado' },
+        { data: series.compensated, type: 'bar', name: 'Energia compensada' }
+      ]
     }
-  }, [isLoad, series])
+  }, [series, isLoad])
 
   return (
     <>
